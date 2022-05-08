5fcc7609164de92a375b34b8f07e337392c2e562
Zentality <jmcp@live.com.au>
2022-05-08 15:30:52 +0800

    Make books appear on web page
    Create a book and it saves to the library array, creation calls
    a function that updates the library on the webpage to display
    all current books in the library.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 let myLibrary = [];
 
+const library = document.querySelector(".library");
+
 const Book = {
-  init: function(author, title, pages, isRead){
-    this.author = author
+  init: function(title, author, pages, isRead){
     this.title = title
+    this.author = author
     this.pages = pages
     this.isRead = isRead
     return this;
@@ -11,18 +13,45 @@ const Book = {
 }
 
 function addBookToLibrary(){
-  let author = prompt("author");
   let title = prompt("title");
+  let author = prompt("author");
   let pages = prompt("pages");
-  let isRead = true;
-  let tempBook = Object.create(Book).init(author, title, pages, isRead);
+  let isRead = prompt("isRead");
+  let tempBook = Object.create(Book).init(title, author, pages, isRead);
   myLibrary.push(tempBook);
+  updateLibraryOnPage();
   return tempBook;
 }
 
- 
+function updateLibraryOnPage(){
+  library.innerHTML = "";
+  myLibrary.forEach(book => {
+    addBookToDOM(book.title, book.author, book.pages, book.isRead);
+  });
+}
 
-addBookToLibrary();
-addBookToLibrary();
+function addBookToDOM(title, author, pages, read){
+  let item = document.createElement("li");
 
-console.log(myLibrary);
+  let itemTitle = document.createElement("div");
+  itemTitle.className = "title";
+  itemTitle.textContent = title;
+
+  let itemAuthor = document.createElement("div");
+  itemAuthor.className = "author";
+  itemAuthor.textContent = author;
+
+  let itemPages = document.createElement("div");
+  itemPages.className = "pages";
+  itemPages.textContent = pages;
+
+  let isRead = document.createElement("div");
+  isRead.className = "isRead";
+  isRead.textContent = read;
+  
+  item.append(itemTitle, itemAuthor, itemPages, isRead);
+  library.appendChild(item);
+}
+
+setTimeout(addBookToLibrary, 1000);
+setTimeout(addBookToLibrary, 1000);
